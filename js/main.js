d9b6b1089db48d725f4c208e0ed91c759c8c8d64
Hunter Caron <huntercaron@me.com>
2016-01-19 11:09:13 -0500

    turnip project now inline

--- js/main.js
+++ js/main.js
@@ -1,8 +1,51 @@
 (function() {
 
-    var isTouchDevice = 'ontouchstart' in document.documentElement;
+    //var isTouchDevice = 'ontouchstart' in document.documentElement;
 
-    
+    var projectOpen = false,
+    turnipButton = document.getElementById("turnip-button"),
+    turnipProject = document.getElementById("turnip-project"),
+    turnipBack = document.getElementById("turnip-back"),
+    grid = document.getElementById("grid"),
+    aboutMore = document.querySelector(".about-more");
+
+
+    turnipButton.addEventListener('click', function() {
+        smoothScroll.animateScroll( null, '#turnip-project' );
+
+        if (!projectOpen) {
+            classie.add(grid, "hide-grid");
+
+            console.log('poop');
+            setTimeout(function(){
+                classie.remove(turnipProject, "hide-project");
+                projectOpen = true;
+                classie.remove(grid, "hide-grid");
+            }, 200);
+        }
+    });
+
+
+    turnipBack.addEventListener('click', function() {
+        classie.add(grid, "hide-grid");
+
+        setTimeout(function(){
+            console.log('poop');
+            classie.remove(grid, "hide-grid");
+            classie.add(turnipProject, "hide-project");
+            projectOpen = false;
+
+        }, 500);
+
+    });
+
+    aboutMore.addEventListener('click', function() {
+        if (projectOpen) {
+            smoothScroll.animateScroll( null, '#about', { offset: -3000});
+        }
+        else
+            smoothScroll.animateScroll( null, '#about' );
+    });
 
 
 })();
