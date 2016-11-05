259aa293ed5c2f8343420c77c3d7a123b9da687f
Hunter Caron <huntercaron@me.com>
2016-11-05 19:15:43 -0400

    pushing grid attempt 1

--- beta/js/main.js
+++ beta/js/main.js
@@ -1,16 +1,16 @@
 var projects = [
-	{name: "turnip", index: 0, open: false, width: "66.666666666", height: "66.666666666", left: 0, top: 0, ele: document.getElementById("pro-" + 0), inner: document.querySelector(".pro-" + 0 + "-inner") },
-	{name: "experiments", index: 1, open: false, width: "33.333333333", height: "33.333333333", left: 2, top: 0, ele: document.getElementById("pro-" + 1), inner: document.querySelector(".pro-" + 1 + "-inner") },
-	{name: "big-data", index: 2, open: false, width: "33.333333333", height: "33.333333333", left: 2, top: 1, ele: document.getElementById("pro-" + 2), inner: document.querySelector(".pro-" + 2 + "-inner") },
-	{name: "sprouted", index: 3, open: false, width: "33.333333333", height: "33.333333333", left: 0, top: 2, ele: document.getElementById("pro-" + 3), inner: document.querySelector(".pro-" + 3 + "-inner") },
-	{name: "companion", index: 4, open: false, width: "66.666666666", height: "33.333333333", left: 1, top: 2, ele: document.getElementById("pro-" + 4), inner: document.querySelector(".pro-" + 4 + "-inner") }
+	{name: "turnip", index: 0, open: false, width: "66.666666666", height: "66.666666666", left: 0, top: 0, ele: document.getElementById("proj-" + 0), inner: document.querySelector(".proj-" + 0 + "-inner") },
+	{name: "experiments", index: 1, open: false, width: "33.333333333", height: "33.333333333", left: 2, top: 0, ele: document.getElementById("proj-" + 1), inner: document.querySelector(".proj-" + 1 + "-inner") },
+	{name: "big-data", index: 2, open: false, width: "33.333333333", height: "33.333333333", left: 2, top: 1, ele: document.getElementById("proj-" + 2), inner: document.querySelector(".proj-" + 2 + "-inner") },
+	{name: "sprouted", index: 3, open: false, width: "33.333333333", height: "33.333333333", left: 0, top: 2, ele: document.getElementById("proj-" + 3), inner: document.querySelector(".proj-" + 3 + "-inner") },
+	{name: "companion", index: 4, open: false, width: "66.666666666", height: "33.333333333", left: 1, top: 2, ele: document.getElementById("proj-" + 4), inner: document.querySelector(".proj-" + 4 + "-inner") }
 ];
 
 var gridEle = document.querySelector(".grid");
 var currentProj = 0;
 
-var currentProjWidth = document.querySelector('.pro-' + currentProj + "-inner").offsetWidth;
-var currentProjHeight = document.querySelector('.pro-' + currentProj + "-inner").offsetHeight;
+var currentProjWidth = document.querySelector('.proj-' + currentProj + "-inner").offsetWidth;
+var currentProjHeight = document.querySelector('.proj-' + currentProj + "-inner").offsetHeight;
 
 var gridWidth = document.querySelector(".grid").offsetWidth;
 var gridHeight = document.querySelector(".grid").offsetHeight;
@@ -41,11 +41,11 @@ function openProject(currentEle) {
 	projects[currentEle.dataset.project].open = true;
     currentProj = currentEle.dataset.project;
 
-	let ele = document.querySelector('.pro-' + currentProj + "-inner");
-	let width = document.querySelector('.pro-' + currentProj + "-inner").offsetWidth;
-	let height = document.querySelector('.pro-' + currentProj + "-inner").offsetHeight;
+	let ele = document.querySelector('.proj-' + currentProj + "-inner");
+	let width = document.querySelector('.proj-' + currentProj + "-inner").offsetWidth;
+	let height = document.querySelector('.proj-' + currentProj + "-inner").offsetHeight;
 
-	currentProjHeight = document.querySelector('.pro-' + currentProj + "-inner").offsetHeight;
+	currentProjHeight = document.querySelector('.proj-' + currentProj + "-inner").offsetHeight;
 
 	gridWidth = document.querySelector(".grid").offsetWidth;
 	gridHeight = document.querySelector(".grid").offsetHeight;
@@ -62,18 +62,19 @@ function openProject(currentEle) {
 							translateY(-${((gridBoxHeight-gridHeight)/2 + 6 + (gridHeight/3)*projects[currentProj].top)/((gridBoxHeight)/height)}px)
 							translateX(-${((gridBoxWidth-gridWidth)/2 + 6 + (gridWidth/3)*projects[currentProj].left)/((gridBoxWidth)/width)}px)`;
 
+
+
 	document.querySelector(".turnip").style.transform = `scale(${((gridBoxHeight)/height-1)})`;
 
-	if (currentProj == 4)
-		document.querySelector(".pro-" + currentProj + "-info").style.transform = `scaleY(${((gridBoxHeight-6)/height-(Math.floor(gridBoxHeight/height)))}) scaleX(${((gridBoxWidth-6)/width-(Math.floor(gridBoxWidth/width)))})`;
-	else
-		document.querySelector(".pro-" + currentProj + "-info").style.transform = `scale(${((gridBoxHeight-6)/height-(Math.floor(gridBoxHeight/height)))})`;
+	document.querySelector(".proj-" + currentProj + "-info").classList.add("hidden-info");
 
 	setTimeout( function(){ document.querySelector(".project-contents").style.transform = "scaleY(1)"; }, 240 );
 	setTimeout( function(){
-		document.querySelector(".turnip").style.transform = `scale(${((gridBoxHeight)/height-1)*0.8}) translateY(-240px)`;
-		document.querySelector(".pro-" + currentProj + "-info").style.transform = document.querySelector(".project-info").style.transform + " translateY(-290px)";
-		document.querySelector(".project-contents").style.transform = "translateY(-300px)"
+		currentEle.classList.add("active-project");
+		document.querySelector(".turnip").style.transform = `scale(${((gridBoxHeight)/height-1)*0.8}) translateY(-${gridBoxHeight/10*3}px)`;
+		document.querySelector(".project-contents").style.transform = `translateY(-${gridBoxHeight/2}px)`
+
+		document.querySelector('.proj-' + currentProj + "-contents").classList.add("show-contents");
 	}, 500);
 
 
@@ -84,20 +85,20 @@ function openProject(currentEle) {
 
 
 function closeProject(currentEle) {
-	let ele = document.querySelector('.pro-' + currentProj + "-inner");
+	let ele = document.querySelector('.proj-' + currentProj + "-inner");
 
 	document.querySelector(".turnip").style.transform = `scale(${((gridBoxHeight)/currentProjHeight-1)})`;
 
-	document.querySelector(".pro-" + currentProj + "-info").style.transform = `scale(${((gridBoxHeight-6)/currentProjHeight-(Math.floor(gridBoxHeight/currentProjHeight)))})`;
-
 	document.querySelector(".project-contents").style.transform = "translateY(0) scaleY(0.0000001)"
 
+	document.querySelector('.proj-' + currentProj + "-contents").classList.remove("show-contents");
 
+	currentEle.classList.remove("active-project");
 
 	setTimeout( function(){
 		document.querySelector(".turnip").style.transform = "scale(1)";
 		ele.style.transform = "";
-		document.querySelector(".pro-" + currentProj + "-info").style.transform = "";
+		document.querySelector(".proj-" + currentProj + "-info").classList.remove("hidden-info");
 	}, 500);
 
 	projects[currentEle.dataset.project].open = false;
