c1450f59a730b5a09b220b6c3b249a0862912c91
Hunter Caron <huntercaron@mac.com>
2017-10-26 11:55:20 -0400

    adding favicon and netlify headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Hunter Caron`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify`
+  ],
 }
