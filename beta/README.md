9848496b3ad96c73b208e1b8e9a3c1046a99ea01
Hunter Caron <huntercaron@mac.com>
2017-10-26 11:49:32 -0400

    intializing gatsby rebuild

--- beta/README.md
+++
@@ -1,2 +0,0 @@
-# boiled
-A simple boilerplate for web projects. Based on skeleton but with my personal flavour and a flexbox grid.
--- beta/watch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /Users/huntercaron/Documents/Personal/huntercaron.github.io
-browser-sync start --server 'beta' --files 'beta'
