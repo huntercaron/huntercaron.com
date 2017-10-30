6bef32017bac0523c3f70299fe4abf31d7990299
Hunter Caron <huntercaron@mac.com>
2017-10-30 18:36:50 -0400

    adding analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    {
+       resolve: `gatsby-plugin-google-analytics`,
+       options: {
+         trackingId: 'UA-60215266-1'
+       },
+     },
     `gatsby-plugin-netlify`
   ],
 }
