d29da6f8ae9f5c4d0dc75a1983bbc181b12ceb17
Hunter Caron <huntercaron@mac.com>
2017-12-16 21:55:35 -0500

    adding margins

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 const Container = styled.div`
   font-family: 'Mod', sans-serif;
   font-weight: 300 !important;
-  margin: 2%;
+  margin: calc(2rem + 2%);
   margin-top: 2rem;
   position: relative;
 
