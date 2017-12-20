4081de26332f4cb9f9c7f53971fb8c2ef5427579
Hunter Caron <huntercaron@mac.com>
2017-12-20 14:15:50 -0500

    gatsby ready to deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,27 @@ module.exports = {
          trackingId: 'UA-60215266-1'
        },
      },
-     `gatsby-transformer-remark`,
+     {
+       resolve: `gatsby-transformer-remark`,
+       options: {
+         plugins: [
+           {
+             resolve: `gatsby-remark-images`,
+             options: {
+               maxWidth: 500,
+             },
+           },
+           {
+             resolve: `gatsby-remark-responsive-iframe`,
+             options: {
+               wrapperStyle: `margin-bottom: 1.0725rem`,
+             },
+           },
+           'gatsby-remark-copy-linked-files',
+           'gatsby-remark-smartypants',
+         ],
+       },
+     },
      `gatsby-plugin-netlify`
   ],
 }
--- src/pages/work/companion.md
+++ src/pages/work/companion.md
@@ -7,6 +7,8 @@ date: 2017-01-04T15:04:10.000Z
 ---
 Methods and properties shown in black are part of the p5.js core, items in blue are part of the p5.dom library. You will need to include an extra file in order to access the blue functions. See the using a library section for information on how to include this library. p5.dom comes with p5 complete or you can download the single file here.
 
-[Case Study](./casestudy.pdf)
+<!-- [Case Study](./casestudy.pdf) -->
 
-![](/img/compainion_thumbnail.png)
+![alt](/img/compainion_thumbnail.png)
+
+![text](/img/compainion_thumbnail.png)
