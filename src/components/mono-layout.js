78cf3ed31a07b548c187a64e8ee9ca58315c3125
Hunter Caron <huntercaron@me.com>
2020-05-12 18:26:51 +0200

    more padding, I hate it

--- src/components/mono-layout.js
+++ src/components/mono-layout.js
@@ -3,14 +3,15 @@ import styled from "styled-components"
 
 const Container = styled.div`
     font-family: "Mod-Mono", sans-serif;
-    padding: 4px 10px;
+    padding: 4px 24px;
     white-space: pre-wrap;
     max-width: 1400px;
-    font-size: 15px;
+    font-size: 14px;
     line-height: 2;
 
     @media (min-width: 600px) {
         padding: 20px 40px;
+        font-size: 14px;
     }
 `
 
