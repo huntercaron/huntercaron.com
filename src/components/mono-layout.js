1dfcef1ddc3e9b745ec3cacec26db475a4792b84
Hunter Caron <huntercaron@me.com>
2020-05-12 18:02:00 +0200

    Mobile padding fix

--- src/components/mono-layout.js
+++ src/components/mono-layout.js
@@ -3,11 +3,15 @@ import styled from "styled-components"
 
 const Container = styled.div`
     font-family: "Mod-Mono", sans-serif;
-    padding: 20px 40px;
+    padding: 8px 16px;
     white-space: pre-wrap;
     max-width: 1400px;
     font-size: 15px;
     line-height: 2;
+
+    @media (min-width: 600px) {
+        padding: 20px 40px;
+    }
 `
 
 export default function MonoLayout({ children }) {
