6f3ffe3db30de2bf80e85c7ce3cb0e3e03beca30
Hunter Caron <huntercaron@me.com>
2020-12-04 10:01:52 +0100

    fuck gatsby

---
+++ main.js
@@ -0,0 +1,92 @@
+// lets try out some modules
+import { h, Component, render } from 'preact';
+import htm from 'htm';
+
+const html = htm.bind(h);
+
+function ListItem(props) {
+    const { text, subText, link } = props
+
+    return html`
+        <li>
+            <span className="bullet"> → </span>
+            <a href=${link} target="_blank" rel="noopener noreferrer">
+                ${text}
+            </a>
+            ${subText && html` — ${subText}`}
+        </li>
+    `
+}
+
+const app = html`
+    <main>
+        <header>
+            <h1>Hunter Caron</h1>
+        </header>
+
+        <div>
+            <section>
+                <p>Designer & sometimes a developer.</p>
+                
+                <p>
+                    I build things for people striving to succeed in what
+                    makes them uniquely brilliant.
+                </p> 
+            </section>
+
+            <section>
+                <h3>Experience</h3>
+                <ul>
+                    <${ListItem}
+                        text="Framer"
+                        subText="Current"
+                        link="https://www.framer.com/"
+                    />
+                    <${ListItem}
+                        text="Messenger"
+                        subText="2018 - 2019"
+                        link="http://facebook.design/"
+                    />
+                    <${ListItem}
+                        text="Messenger (Intern)"
+                        subText="2017"
+                        link="http://facebook.design/"
+                    />
+                    <${ListItem}
+                        text="Format"
+                        subText="2016 – 2017"
+                        link="https://format.com/"
+                    />
+                    <${ListItem}
+                        text="Konrad Group"
+                        subText="2016"
+                        link="http://www.konradgroup.com/"
+                    />
+                </ul>
+            </section>
+
+            <section>
+                <h3>Contact</h3>
+                <ul>
+                    <${ListItem}
+                        text="Email"
+                        link="mailto:huntercaron.design@icloud.com"
+                    />
+                    <${ListItem}
+                        text="Twitter"
+                        link="https://twitter.com/huntercaron"
+                    />
+                    <${ListItem}
+                        text="Instagram"
+                        link="https://www.instagram.com/hunterhcaron/"
+                    />
+                    <${ListItem}
+                        text="Github"
+                        link="https://github.com/huntercaron"
+                    />
+                </ul>
+            </section>
+        </div>
+    </main>
+`;
+render(app, document.body);
--- src/pages/work/test.md
+++
@@ -1,8 +0,0 @@
----
-templateKey: work-page
-path: /test
-date: 2018-01-10T02:02:29.872Z
-title: test
-subtitle: test
----
-hello test?
